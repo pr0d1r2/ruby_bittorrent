95d54bc0dd469b287efe98a064ae9bdc24a63255
Marcin Nowicki <pr0d1r2@gmail.com>
2013-08-17 00:25:48 +0200

    Fix coverage checks.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,11 @@
+require 'simplecov'
+SimpleCov.start
+SimpleCov.minimum_coverage 100
+
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'ruby_bittorrent'
-
-require 'simplecov'
-SimpleCov.start
-SimpleCov.minimum_coverage 100
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
